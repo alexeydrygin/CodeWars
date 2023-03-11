87479afd15d1b5f5f5672903706ce103f5b83bde
drygin_alexey <drygin.alex@mail.ru>
2023-03-11 18:18:27 +0700

    cw

--- Python/6_Kebabize/main.py
+++ Python/6_Kebabize/main.py
@@ -1,1 +1,11 @@
 # 6 Kyu - Kebabize
+def kebabize(string):
+    # удаляем все цифры из строки
+    string = ''.join(c for c in string if not c.isdigit())
+    # добавляем '-' перед каждой заглавной буквой, кроме первой
+    string = ''.join('-' + c.lower() if c.isupper() and i !=
+                     0 else c.lower() for i, c in enumerate(string))
+    # удаляем первый символ, если он является '-'
+    if string and string[0] == '-':
+        string = string[1:]
+    return string
