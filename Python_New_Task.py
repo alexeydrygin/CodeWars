9a5864c6a223f348e5d27da4ebffc34f6e16a46c
drygin_alexey <drygin.alex@mail.ru>
2023-03-27 12:10:29 +0700

    modified

--- Python_New_Task.py
+++ Python_New_Task.py
@@ -4,102 +4,61 @@ import requests
 from googletrans import Translator
 from bs4 import BeautifulSoup
 import re
-import time
 import sys
 
-# очистить содержимое окна консоли
-os.system('cls' if os.name == 'nt' else 'clear')
 
-# Проверка соединения с интернетом
-try:
+os.system('cls' if os.name == 'nt' else 'clear') # очистить содержимое окна консоли
+
+try: # Проверка соединения с интернетом
     requests.get('https://www.codewars.com/')
-    text = "Подключение к CodeWars: "
-    animation = "|/-\\"
-    for i in range(5):
-        for char in animation:
-            sys.stdout.write('\r' + text + ' ' + char)
-            sys.stdout.flush()
-            time.sleep(0.1)
-    sys.stdout.write('\r' + text + ' ' + 'Готово!' + '\n')
+    text = "Подключение к CodeWars: Готово!"
+    sys.stdout.write('\r' + text + '\n')
 except requests.exceptions.RequestException as e:
     print('Отсутствует подключение к интернету. Проверьте ваше подключение и повторите попытку.')
     exit()
 
-# Введите URL-адрес страницы задачи CodeWars здесь
-url = input("\nВведите URL задачи: ")
+url = input("\nВведите URL задачи: ") # Введите URL-адрес страницы задачи CodeWars здесь
 
-# Извлекаем ID задачи из URL-адреса
-kata_id = url.split('/')[-1]
+kata_id = url.split('/')[-1] # Извлекаем ID задачи из URL-адреса
 
-# Создаем API-запрос, чтобы получить данные о задаче
-response = requests.get(f'https://www.codewars.com/api/v1/code-challenges/{kata_id}')
+response = requests.get(f'https://www.codewars.com/api/v1/code-challenges/{kata_id}') # Создаем API-запрос, чтобы получить данные о задаче
 html = response.content
 
-# создаем объект BeautifulSoup
-soup = BeautifulSoup(html, "html.parser")
+soup = BeautifulSoup(html, "html.parser") # создаем объект BeautifulSoup
 
-# ищем элементы, содержащие информацию о ранге и названии задачи
-# rank_element = response.json().get('rank')
-# if rank_element:
-#     rank_text = rank_element.get('name')
-#     if not rank_text:
-#         rank_text = "0 kyu"
-# else:
-#     rank_text = 'unknown rank'
-# title_element = response.json()['name']
-# if title_element:
-#     title_text = title_element
-# else:
-#     title_text = 'unknown title'
-
-rank_element = response.json().get('rank')
+rank_element = response.json().get('rank') # ищем элементы, содержащие информацию о ранге и названии задачи
 rank_text = rank_element.get('name') if rank_element else "0 kyu"
 title_text = response.json()['name'] if response.json()['name'] else 'unknown title'
 
+prefix = "## " + f"{rank_text}" + " - " + f"{title_text}\n" + "[Ссылка на задачу](" + f"{url}" + ' "' + f"{title_text}" + '")\n\n' # объединяем текст ранга и названия задачи с ссылкой на задачу
 
-# объединяем текст ранга и названия задачи с ссылкой на задачу
-prefix = "## " + f"{rank_text}" + " - " + f"{title_text}\n" + "[Ссылка на задачу](" + f"{url}" + ' "' + f"{title_text}" + '")\n\n'
+kata_description_en = response.json()['description'] # Извлекаем описание задачи из ответа API
 
-# Извлекаем описание задачи из ответа API
-kata_description_en = response.json()['description']
-
-#заменить все вхождения подстроки http на https в переменной kata_description_en
-if 'http' in kata_description_en:
+if 'http' in kata_description_en: #заменить все вхождения подстроки http на https в переменной kata_description_en
     kata_description_en = kata_description_en.replace('http', 'https')
 
-# добавляем префикс в начало kata_description_en
-kata_description_en = prefix + kata_description_en
+kata_description_en = prefix + kata_description_en # добавляем префикс в начало kata_description_en
 
-# Создаем экземпляр класса Translator
-translator = Translator()
+translator = Translator() # Создаем экземпляр класса Translator
 
-# Переводим описание задачи на русский язык
-kata_description_ru = translator.translate(kata_description_en, dest='ru').text
+kata_description_ru = translator.translate(kata_description_en, dest='ru').text # Переводим описание задачи на русский язык
 
-#заменить все вхождения подстроки `` ` на ``` в переменной kata_description_ru
-if '`` `' in kata_description_ru:
+if '`` `' in kata_description_ru: #заменить все вхождения подстроки `` ` на ``` в переменной kata_description_ru
     kata_description_ru = kata_description_ru.replace('`` `', '```')
 
-# Добавляем дополнительный текст в конец описания задачи на русском языке
-kata_description_ru += '\n\n---\n\nВведите описание решения задачи здесь.'
+kata_description_ru += '\n\n---\n\nВведите описание решения задачи здесь.' # Добавляем дополнительный текст в конец описания задачи на русском языке
 
-# номер сложности из ранга 
-difficulty = int(re.findall('\d+', rank_text)[0])
+difficulty = int(re.findall('\d+', rank_text)[0]) # номер сложности из ранга 
 
-# Замена элементов пунктуации и пробелов на _
 folder_name = ''.join(
-    c if c not in string.punctuation and not c.isspace() else '_' for c in title_text)
+    c if c not in string.punctuation and not c.isspace() else '_' for c in title_text) # Замена элементов пунктуации и пробелов на _
 
-# Создание папки с заданным номером сложности и названием
-folder_path = f"Python/{difficulty}_{folder_name}"
+folder_path = f"Python/{difficulty}_{folder_name}" # Создание папки с заданным номером сложности и названием
 os.makedirs(folder_path)
 
-# Создание файлов readme.md и main.py в созданной папке
-with open(os.path.join(folder_path, "readme.md"), "w", encoding = "UTF-8") as f:
-    # первая строка начинается с ## и содержит название папки
-    f.write(kata_description_en + "\n\n---\n## Задание:\n\n" + kata_description_ru)
+with open(os.path.join(folder_path, "readme.md"), "w", encoding = "UTF-8") as f: # Создание файлов readme.md и main.py в созданной папке
+    f.write(kata_description_en + "\n\n---\n## Задание:\n\n" + kata_description_ru) # первая строка начинается с ## и содержит название папки
 with open(os.path.join(folder_path, "main.py"), "w") as f:
-    # первая строка начинается с # и содержит название папки
-    f.write("# " + rank_text +" - " + title_text + "\n")
+    f.write("# " + rank_text +" - " + title_text + "\n") # первая строка начинается с # и содержит название папки
 
 print(f"\nПапка {difficulty}_{folder_name} создана успешно.")
